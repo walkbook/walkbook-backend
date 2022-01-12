5f67bde27e516007b9c6d86c858bbc5f241f1940
sewonK <cutecat1128@naver.com>
2022-01-12 21:50:31 +0900

    [Feat] Add daum.html(Address)

---
+++ src/main/java/walkbook/server/config/MvcConfig.java
@@ -0,0 +1,18 @@
+package walkbook.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/templates/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+    }
+}
