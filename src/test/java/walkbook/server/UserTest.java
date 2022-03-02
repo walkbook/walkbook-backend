081547024e061c6f5444eed9c2563f49cd711642
sewonK <cutecat1128@naver.com>
2022-03-02 23:03:17 +0900

    [Test] Add User Test - 회원정보수정 성공/실패 테스트 추가 - getUserRequest Extract method

--- src/test/java/walkbook/server/UserTest.java
+++ src/test/java/walkbook/server/UserTest.java
@@ -1,5 +1,6 @@
 package walkbook.server;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,16 +9,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import walkbook.server.advice.exception.CLoginFailedException;
+import walkbook.server.advice.exception.CSignupFailedException;
+import walkbook.server.advice.exception.CUserNotFoundException;
 import walkbook.server.domain.Gender;
 import walkbook.server.domain.User;
 import walkbook.server.dto.user.UserRequest;
+import walkbook.server.jwt.JwtTokenUtil;
 import walkbook.server.repository.UserRepository;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,6 +43,10 @@ public class UserTest {
     private ObjectMapper objectMapper;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
 
     @BeforeEach
     public void Init() {
@@ -44,7 +57,7 @@ public class UserTest {
                 .gender(Gender.M)
                 .age("20")
                 .location("서울시 관악구")
-                .introduction("안녕하세요. 테스트입니다.")
+                .introduction("초기값")
                 .build());
         userId = Math.toIntExact(user.getUserId());
     }
@@ -57,18 +70,10 @@ public class UserTest {
     @Test
     public void 회원가입_성공() throws Exception {
         //given
-        String object = objectMapper.writeValueAsString(UserRequest.builder()
-                .username("tester")
-                .password(passwordEncoder.encode("tester"))
-                .nickname("테스터")
-                .gender(Gender.F)
-                .age("25")
-                .location("서울시 용산구")
-                .introduction("테스트")
-                .build());
+        String userRequest = getUserRequest("tester", "tester", "tester", Gender.M, "20", "서울시 용산구", "테스트");
         //when
         ResultActions actions = mockMvc.perform(post("/api/user/signup")
-                .content(object)
+                .content(userRequest)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON));
 
@@ -82,39 +87,29 @@ public class UserTest {
     @Test
     public void 회원가입_실패_중복회원() throws Exception {
         //given
-        String object = objectMapper.writeValueAsString(UserRequest.builder()
-                .username("admin")
-                .password(passwordEncoder.encode("admin"))
-                .nickname("admin")
-                .gender(Gender.F)
-                .age("20")
-                .location("서울시 강남구")
-                .introduction("중복테스트")
-                .build());
+        String userRequest = getUserRequest("admin", "admin", "", Gender.M, "", "", "");
         //when
         ResultActions actions = mockMvc.perform(post("/api/user/signup")
-                .content(object)
+                .content(userRequest)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON));
 
         //then
         actions
                 .andDo(print())
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.success").value(false));
+                .andExpect(result -> assertTrue(result.getResolvedException().getClass().isAssignableFrom(CSignupFailedException.class)))
+                .andExpect(status().isBadRequest());
+
     }
 
     @Test
     public void 로그인_성공() throws Exception {
         //given
-        String object = objectMapper.writeValueAsString(UserRequest.builder()
-                .username("admin")
-                .password("admin")
-                .build());
+        String userRequest = getUserRequest("admin", "admin", "", Gender.M, "", "", "");
 
         //when
         ResultActions actions = mockMvc.perform(post("/api/user/signin")
-                .content(object)
+                .content(userRequest)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON));
 
@@ -128,10 +123,7 @@ public class UserTest {
     @Test
     public void 로그인_실패() throws Exception {
         //given
-        String object = objectMapper.writeValueAsString(UserRequest.builder()
-                .username("tester")
-                .password("tester")
-                .build());
+        String object = getUserRequest("tester", "tester", "tester", Gender.M, "20", "서울시 용산구", "테스트");
 
         //when
         ResultActions actions = mockMvc.perform(post("/api/user/signin")
@@ -142,8 +134,8 @@ public class UserTest {
         //then
         actions
                 .andDo(print())
-                .andExpect(status().is4xxClientError())
-                .andExpect(jsonPath("$.code").value("-1001"));
+                .andExpect(result -> assertTrue(result.getResolvedException().getClass().isAssignableFrom(CLoginFailedException.class)))
+                .andExpect(status().is4xxClientError());
     }
 
     @Test
@@ -168,8 +160,68 @@ public class UserTest {
         //then
         actions
                 .andDo(print())
-                .andExpect(status().isBadRequest())
-                //-1000, This member does not exist.
-                .andExpect(jsonPath("$.code").value("-1000"));
+                .andExpect(result -> assertTrue(result.getResolvedException().getClass().isAssignableFrom(CUserNotFoundException.class)))
+                .andExpect(status().isBadRequest());
+    }
+
+
+    @Test
+    public void 회원정보수정_성공() throws Exception {
+        //given
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        "admin",
+                        "admin"
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        final String token = jwtTokenUtil.generateToken(authentication);
+
+        String userRequest = getUserRequest("tester", "tester", "tester", Gender.M, "20", "서울시 용산구", "테스트");
+
+        //when
+        ResultActions actions = mockMvc.perform(put("/api/user/{userId}", userId)
+                .header("X-AUTH-TOKEN", token)
+                .content(userRequest)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        actions
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.userId").value(userId))
+                .andExpect(jsonPath("$.data.nickname").value("tester"))
+                .andExpect(jsonPath("$.data.location").value("서울시 용산구"))
+                .andExpect(jsonPath("$.data.introduction").value("테스트"));
+    }
+
+    @Test
+    public void 회원정보수정_실패_권한없음() throws Exception {
+        //given
+        String userRequest = getUserRequest("tester", "tester", "tester", Gender.M, "20", "서울시 용산구", "테스트");
+
+        //when
+        ResultActions actions = mockMvc.perform(put("/api/user/{userId}", userId)
+                .content(userRequest)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        //then
+        actions
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    private String getUserRequest(String username, String password, String nickname, Gender gender, String age, String location, String introduction) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(UserRequest.builder()
+                .username(username)
+                .password(password)
+                .nickname(nickname)
+                .gender(gender)
+                .age(age)
+                .location(location)
+                .introduction(introduction)
+                .build());
     }
 }
